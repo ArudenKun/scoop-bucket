3a887bea462e1274ebefea36f8fec7a3369f8107
Aruden <84431560+ArudenKun@users.noreply.github.com>
2022-09-09 22:03:11 +0800

    asdas

--- scripts/maven/repository.ps1
+++ scripts/maven/repository.ps1
@@ -5,6 +5,7 @@ if (!(Test-Path "$persist_dir\conf\settings.xml")) {
     $current = (Split-Path $dir | Join-Path -ChildPath 'current') -replace '\\', '/'
 
     $repository = Get-Content "$dir\conf\settings.xml"
+    Write-Information($repository)
     $repository[54] = "`n  <localRepository>`"`$1$current\\data\\repository`"</localRepository>`n"
     Set-Content -LiteralPath $repository -Value $repository
 }
