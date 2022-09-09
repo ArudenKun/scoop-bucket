1a081d740d8b43ce1a95841d948dd0a85039c3cc
Aruden <84431560+ArudenKun@users.noreply.github.com>
2022-09-09 22:06:05 +0800

    asdas

--- scripts/maven/repository.ps1
+++ scripts/maven/repository.ps1
@@ -6,5 +6,6 @@ if (!(Test-Path "$persist_dir\conf\settings.xml")) {
 
     $repository = Get-Content "$dir\conf\settings.xml"
     $repository[54] = "`n  <localRepository>`$1$current\\data\\repository</localRepository>`n"
+
     Set-Content -LiteralPath "$dir\conf\settings.xml" -Value $repository
 }
