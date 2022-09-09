2cfe5ac5ac5b69167902d38070ec8fcea7ede5ed
Aruden <84431560+ArudenKun@users.noreply.github.com>
2022-09-09 22:01:02 +0800

    asdsa

--- scripts/maven/repository.ps1
+++ scripts/maven/repository.ps1
@@ -1,4 +1,10 @@
-param([Parameter(Mandatory)][String] $settings, [Parameter(Mandatory)][String] $dir)
-$CONST = Get-Content $settings
-$CONST[54] = "`n  <localRepository>`"$dir\\data\\repository`"</localRepository>`n"
-Set-Content -LiteralPath $settings -Value $CONST
+param([Parameter(Mandatory)][String] $dir, [Parameter(Mandatory)][String] $persist_dir)
+
+if (!(Test-Path "$persist_dir\conf\settings.xml")) {
+
+    $current = (Split-Path $dir | Join-Path -ChildPath 'current') -replace '\\', '/'
+
+    $repository = Get-Content "$dir\conf\settings.xml"
+    $repository[54] = "`n  <localRepository>`"`$1$current\\data\\repository`"</localRepository>`n"
+    Set-Content -LiteralPath $repository -Value $repository
+}
