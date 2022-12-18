792e1a32ca9e65cd085fb4c1791e8d8832adeb31
Aruden <84431560+ArudenKun@users.noreply.github.com>
2022-12-18 21:55:13 +0800

    asd

---
+++ scripts/microsoft/vscode/code.cmd
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+set VSCODE_APPDATA=%~dp0data\appdata
+set VSCODE_LOGS=%~dp0data\logs
+set VSCODE_EXTENSIONS=%~dp0data\extensions
+
+set VSCODE_DEV=
+set ELECTRON_RUN_AS_NODE=1
+
+"%~dp0app\Code.exe" "%~dp0app\resources\app\out\cli.js" --ms-enable-electron-run-as-node %*
+endlocal
