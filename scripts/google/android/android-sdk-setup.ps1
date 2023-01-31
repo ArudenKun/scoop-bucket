b05d4bde52c9ae33bb8713ae6ff9ca74cecfee44
Aruden <84431560+ArudenKun@users.noreply.github.com>
2023-01-31 18:15:21 +0800

    asdada

--- scripts/google/android/android-sdk-setup.ps1
+++ scripts/google/android/android-sdk-setup.ps1
@@ -147,6 +147,10 @@ if ($platform_installed -eq $false) {
     Write-Host "No platform detected. Installing the latest version... ($latest_platforms)" -ForegroundColor Yellow
     # sdkmanager.bat $latest_platforms
     Write-Host $latest_platforms
+
+    foreach ($item in $platforms) {
+        Write-Host $item
+    }
 }
 
 # Done. We should be able to develop for Android now.
