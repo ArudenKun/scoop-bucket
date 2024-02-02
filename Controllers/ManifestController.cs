6d9dfc09a9ea055cc7c3c245428c5146c1449ad8
Alden Manisan <84431560+ArudenKun@users.noreply.github.com>
2024-02-02 18:52:56 +0800

    Revert back to dotnet

---
+++ Controllers/ManifestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScoopBucketApi.Controllers;
+
+[ApiController]
+[Route("api/{id}")]
+public sealed class ManifestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<App> Get(string id)
+    {
+        return new App(id, id, id, id);
+        // return new App("", "", "", "");
+    }
+}
+
+public record App(string Name, string Url, string Version, string Hash);
--- src/manifest/manifest.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from "@nestjs/common";
-
-@Controller("manifest")
-export class ManifestController {
-  @Get()
-  test(): string {
-    return "Manifest";
-  }
-}
